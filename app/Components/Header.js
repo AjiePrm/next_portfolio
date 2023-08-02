b2c236677c779858eaeb6bd6d2a3b27dcc6b630e
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-08-02 22:15:32 +0700

    Finish hero part right side, need add navbar animation

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -5,11 +5,11 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className='pl-[48px] grid grid-cols-2 h-[30px]'>
+    <div className='pl-[48px] grid grid-cols-2 h-[30px] z-0'>
         <div className='logo pt-[48px] cursor-pointer'>
            <Image src='./logoAjie.svg' alt="japan" width={52} height={52}/> 
         </div>
-        <nav>
+        <nav className='z-0'>
           <ul className='text-secondaryDark text-[18px] font-normal 
           flex space-x-[11px] justify-end pr-[50px] pt-[30px]'>
             <li className='hover:text-primaryRed hover:cursor-pointer
--- app/Components/Hero.js
+++ app/Components/Hero.js
@@ -5,32 +5,39 @@ export default function Hero() {
   return (
     <div className='pl-[48px] pt-[165px] grid grid-cols-2'>
         <div className='left'>
-            <div>
+            <div className='cursor-not-allowed'>
                 <h1 className='text-secondaryDark font-bold text-[42px] leading-[50px] not-italic'>いらっしゃいませ <br/>
                     <span className='text-primaryRed'>INDEPENDENT FRONT-END</span> <br/>
                     WEB DEVELOPER
                 </h1>
             </div> 
-            <div className='pt-[34px]'>
+            <div className='pt-[34px] cursor-not-allowed'>
                 <p className='text-Paragraph font-normal text-[18px]'>
                 Hey ! I’m Ajie, twenty-three years old front-end developer. I make <br/> 
                 beautiful website that help you achieve your goals. Also, i’m <br/>
                 interested in web design and art.
                 </p>
             </div>
-            <div className='mt-[97px] ml-[4px] relative h-[47px]'>
-                <div className='hover:cursor-pointer
-                active:ml-[-4px] active:bottom-[-4px]'>
-                    <button className='py-[11.5px] px-[26px] bg-primaryRed rounded-[4px]'>
-                        My Curculum Vitae
+            <div className='relative h-[47px] hover:cursor-pointer mt-[97px] ml-[4px]'>
+                <div className='absolute pl-[4px] active:bottom-[-4px] active:pl-[0px] active:ml-[0px] z-20'>
+                    <button className='py-[11.5px] px-[26px] bg-primaryRed rounded-[4px] z-21'>
+                        My Curriculum Vitae
                     </button>
                 </div>
-                <div className='h-[47px] w-[195px] absolute border border-primaryRed left-[-4px] bottom-[-4px]'> 
+                <div className='h-[47px] w-[204px] shadow-md absolute border border-primaryRed bottom-[-4px] z-0 rounded-[4px]'> 
                     
                 </div>
                 
             </div>
         </div>
+
+        <div className='relative w-fit z-20'>
+           <div className='w-[428px] h-[428px] bg-primaryRed rounded-[50%] ml-[22px] mt-4 shadow-md '>
+           </div>
+           <div className='absolute top-[-80px] left-[-60px] w-[634px] z-20'>
+            <Image className='z-20' src='/hero.png' alt="Ajie" width={600} height={588}/>
+           </div>
+        </div> 
 
     </div>
   )
