e18d6d6fdc3e49bf5efea594af35fc628c04d9e9
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-08-14 22:00:14 +0700

    initiate sec page

---
+++ app/Components/Project.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Project() {
+  return (
+    <div className='pl-[48px] pt-[165px]'>
+      <h1 className='text-center text-secondaryDark text-[42px]'>ミニプロジェクト <br/>
+        MINI PROJECT</h1>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Header from './Components/Header'
 import Hero from './Components/Hero'
+import Project from './Components/Project'
 
 export default function Home() {
   return (
@@ -7,7 +8,13 @@ export default function Home() {
       <div className='w-[1262px] h-[833px] border rounded-[20px] flex items-center justify-between'>
         <div className="w-[1216px] h-[781px] bg-PrimaryCream rounded-[20px] m-[20px]">
           <Header></Header>
-          <Hero></Hero>
+          <div className='hidden'>
+            <Hero></Hero>   
+          </div>
+          <div>
+            <Project></Project>
+          </div>
+         
         </div>
       </div>
     </main>
